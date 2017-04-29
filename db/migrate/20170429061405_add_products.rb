f347f1b0ebb4fe929ba4782383c327b2d4416352
ol <and-new@rambler.ru>
2017-04-29 14:03:03 +0600

    Update end  les 32 an begin les 33

---
+++ db/migrate/20170429061405_add_products.rb
@@ -0,0 +1,30 @@
+class AddProducts < ActiveRecord::Migration[5.1]
+  def change
+  	Product.create :title => 'p1',
+  				   :description => 'This is Hawaiinan pizza',
+  				   :price => 350,
+  				   :size => 30,
+  				   :is_spicy => false,
+  				   :is_veg => false,
+  				   :is_best_offer => false,
+  				   :path_to_image => '/images/p1.jpg'
+
+  	Product.create :title => 'p2',
+  				   :description => 'Nice Pepperoni pizza',
+  				   :price => 450,
+  				   :size => 30,
+  				   :is_spicy => false,
+  				   :is_veg => false,
+  				   :is_best_offer => true,
+  				   :path_to_image => '/images/p2.jpg'
+
+  	Product.create :title => 'p3',
+  				   :description => 'Amazing Vegetarian pizza',
+  				   :price => 400,
+  				   :size => 30,
+  				   :is_spicy => false,
+  				   :is_veg => false,
+  				   :is_best_offer => false,
+  				   :path_to_image => '/images/p3.jpg'			
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428135727) do
+ActiveRecord::Schema.define(version: 20170429061405) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
