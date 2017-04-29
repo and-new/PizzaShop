ed1e339b0de5973907eed55c09bd7532ca53cd89
ol <and-new@rambler.ru>
2017-04-29 14:39:51 +0600

    Display products step3

--- db/migrate/20170429061405_add_products.rb
+++ db/migrate/20170429061405_add_products.rb
@@ -16,7 +16,7 @@ class AddProducts < ActiveRecord::Migration[5.1]
   				   :is_spicy => false,
   				   :is_veg => false,
   				   :is_best_offer => true,
-  				   :path_to_image => '/images/p2.jpg'
+  				   :path_to_image => '/images/p22.jpg'
 
   	Product.create :title => 'p3',
   				   :description => 'Amazing Vegetarian pizza',
@@ -25,6 +25,6 @@ class AddProducts < ActiveRecord::Migration[5.1]
   				   :is_spicy => false,
   				   :is_veg => false,
   				   :is_best_offer => false,
-  				   :path_to_image => '/images/p3.jpg'			
+  				   :path_to_image => '/images/p33.jpg'			
   end
 end
