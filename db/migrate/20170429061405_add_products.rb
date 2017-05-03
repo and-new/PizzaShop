f0e6dc56ae8e470dd52ab21885885308991fde60
ol <and-new@rambler.ru>
2017-05-03 13:12:11 +0600

    Add cart page

--- db/migrate/20170429061405_add_products.rb
+++ db/migrate/20170429061405_add_products.rb
@@ -1,6 +1,6 @@
 class AddProducts < ActiveRecord::Migration[5.1]
   def change
-  	Product.create :title => 'p1',
+  	Product.create :title => 'Hawaiian',
   				   :description => 'This is Hawaiinan pizza',
   				   :price => 350,
   				   :size => 30,
@@ -9,7 +9,7 @@ class AddProducts < ActiveRecord::Migration[5.1]
   				   :is_best_offer => false,
   				   :path_to_image => '/images/p1.jpg'
 
-  	Product.create :title => 'p2',
+  	Product.create :title => 'Pepperoni',
   				   :description => 'Nice Pepperoni pizza',
   				   :price => 450,
   				   :size => 30,
@@ -18,7 +18,7 @@ class AddProducts < ActiveRecord::Migration[5.1]
   				   :is_best_offer => true,
   				   :path_to_image => '/images/p2.jpg'
 
-  	Product.create :title => 'p3',
+  	Product.create :title => 'Vegetarian',
   				   :description => 'Amazing Vegetarian pizza',
   				   :price => 400,
   				   :size => 30,
